b84f728511890727969d889e5ff6e9b89fef05a8
Trang Nguyen <trnguyen@student.hive.fi>
2021-12-13 15:21:05 +0200

    fixed strtrim memory leak

--- libft/ft_strtrim.c
+++ libft/ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: trnguyen <trnguyen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/15 19:13:59 by trnguyen          #+#    #+#             */
-/*   Updated: 2021/12/12 23:49:42 by trnguyen         ###   ########.fr       */
+/*   Updated: 2021/12/13 15:15:15 by trnguyen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,20 @@
 
 char	*ft_strtrim(char const *s)
 {
-	unsigned int	i;
-	unsigned int	j;
-	char			*res;
+	size_t	start;
+	size_t	end;
+	char	*res;
 
-	if (!s || !*s)
-		return ("\0");
-	i = 0;
-	j = ft_strlen(s) - 1;
-	while (s[i] == ' ' || s[i] == '\n' || s[i] == '\t')
-		i++;
-	while ((s[j] == ' ' || s[j] == '\n' || s[j] == '\t') && j != 0)
-		j--;
-	res = (char *)malloc(sizeof(char) * (j - i + 2));
-	if (res == NULL)
+	if (!s)
 		return (NULL);
-	res = ft_strsub(s, i, j - i + 1);
+	start = 0;
+	end = ft_strlen(s) - 1;
+	while (s[start] == ' ' || s[start] == '\n' || s[start] == '\t')
+		start++;
+	while ((s[end] == ' ' || s[end] == '\n' || s[end] == '\t') && end != 0)
+		end--;
+	if (start >= end)
+		return (ft_strdup(""));
+	res = ft_strsub(s, start, end - start + 1);
 	return (res);
 }
