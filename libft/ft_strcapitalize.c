c51b05c3a319fabfbfa8a2393ea43de50b17267b
Trang Nguyen <trnguyen@student.hive.fi>
2021-12-14 15:27:33 +0200

    strcap

--- libft/ft_strcapitalize.c
+++ libft/ft_strcapitalize.c
@@ -6,36 +6,44 @@
 /*   By: trnguyen <trnguyen@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/13 18:54:09 by trnguyen          #+#    #+#             */
-/*   Updated: 2021/12/13 20:37:19 by trnguyen         ###   ########.fr       */
+/*   Updated: 2021/12/14 15:27:18 by trnguyen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+// #include "stdio.h"
 
 char	*ft_strcapitalize(char *str)
 {
-	size_t	i;
-
+	size_t	i, j;
 	i = 0;
+	j = 1;
+	str = ft_strlowcase(str);
 	while (str[i])
 	{
-		while (!ft_isalpha(str[i]))
-			i++;
-		if (ft_islower(str[i]))
-			str[i] = ft_toupper(str[i]);
-		while (!ft_isspace(str[i]))
+		if (ft_islower(str[i])) //(str[i] >= 'a' && str[i] <= 'z')
 		{
-			str[i] = ft_tolower(str[i]);	
-			i++;
+			if (j == 1)
+				str[i] = ft_toupper(str[i]);
+			j = 0;
 		}
+		else if (ft_isdigit(str[i])) //(str[i] >= '0' && str[i] <= '9')
+			j = 0;
+		else
+			j = 1;
+		i++;
 	}
-	return (str);
-}
 
-int main (int ac, char **av)
-{
-	if (ac == 2)
-	{
-		ft_putendl(ft_strcapitalize(av[1]));
-	}
+	// {
+	// 	while (!ft_isalnum(str[i]))
+	// 		i++;
+	// 	if (ft_isalnum(str[i]) && (!ft_isalnum(str[i-1]) || str[i - 1])
+	// 	{
+	// 		str[i] = ft_toupper(str[i]);
+	// 		i++;
+	// 	}
+	// 	while (ft_isalnum(str[i]) && str[i])
+	// 		i++;
+	// }
+	return (str);
 }
